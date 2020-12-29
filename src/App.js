80e9025c00dd855c39ef371900233874c35cb1fb
adnba <39657038+adnba@users.noreply.github.com>
2020-12-29 14:43:43 +0100

    added stuff

--- src/App.js
+++ src/App.js
@@ -3,12 +3,46 @@ import "./styles.css"
 import Todo from "./components/Todo"
 
 class App extends Component {
+  state = {
+    todoList: ["eat", "dance", "eat", "sleep", "sleep"],
+    value: ""
+  }
+
+  addItem = (e) => {
+    e.preventDefault()
+
+    this.setState((prevState) => {
+      const newList = prevState.todoList.concat()
+      newList.push(prevState.value)
+      return {
+        todoList: newList
+      }
+    })
+  }
+
+  changeValue = (e) => {
+    const { value } = e.target
+    this.setState({
+      value
+    })
+  }
+
   render() {
-    console.log('render')
-    const todoList = ['eat', 'dance', 'eat', 'sleep']
+    console.log("render")
+    console.log("value", this.state.value)
     return (
       <div className="container">
-        <Todo todoList={todoList}/>
+        <form onSubmit={this.addItem}>
+          <label htmlFor="input">Todo</label>
+          <input
+            type="text"
+            id="input"
+            onChange={this.changeValue}
+            value={this.state.value}
+          />
+          <button onClick={this.addItem}>add item</button>
+        </form>
+        <Todo todoList={this.state.todoList} />
       </div>
     )
   }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,18 +3,14 @@ import React, { Component } from "react"
 class Todo extends Component {
   render() {
     console.log(this.props.todoList)
-    const todoListElements = this.props.todoList.map((todoItem) => (
-      <li>{todoItem}</li>
+    const todoListElements = this.props.todoList.map((todoItem, index) => (
+      <li key={index}>{todoItem}</li>
     ))
 
     return (
       <>
-        <label htmlFor="input">Todo</label>
-        <input type="text" id="input" />
-        <div>
-          <div>Items</div>
-          <ul>{todoListElements}</ul>
-        </div>
+        <div>Items</div>
+        <ul>{todoListElements}</ul>
       </>
     )
   }
